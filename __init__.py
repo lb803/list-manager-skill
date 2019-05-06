6b34ba23c2ed628b9a2288651251b4ce74c7ace4
Luca Baffa <lb803@mailbox.org>
2019-05-06 21:30:51 +0100

    Minor improvements and bug fixes
    Cleaned up the code from some redundant instructions and fixed minor issues.

--- __init__.py
+++ __init__.py
@@ -38,28 +38,28 @@ class ListManager(MycroftSkill):
     def handle_read(self, message):
         """ Read items on a specific list or read all the lists names"""
 
-        list_name = message.data.get('list_name')
+        data = {'list_name': message.data.get('list_name')}
 
-        if list_name:
+        if data['list_name']:
             # If the user specified a list name, read items on that list
-            if self.db.list_empty(list_name):
-                self.speak_dialog('no.items', {'list_name': list_name})
-            elif not self.db.list_exists(list_name):
-                self.speak_dialog('list.not.found', {'list_name': list_name})
+            if self.db.list_empty(data['list_name']):
+                self.speak_dialog('no.items', data)
+            elif not self.db.list_exists(data['list_name']):
+                self.speak_dialog('list.not.found', data)
             else:
-                data = {'list': list_name,
-                        'items': self.string(self.db.read_items(list_name))}
-    
+                data['items'] = self.string(self.db.read_items(data['list_name']))
+
                 self.speak_dialog('items.in.list', data)
+
         else:
             # Alternatively, simply read lists names
             if self.db.no_lists():
                 self.speak_dialog('no.lists')
             else:
                 lists = self.db.read_lists()
-                data = {'list_names': self.string(lists),
-                        'list': self.plural_singular_form(lists)}
-    
+                data['list_names'] = self.string(lists)
+                data['list'] = self.plural_singular_form(lists)
+
                 self.speak_dialog('list.lists', data)
 
     @intent_handler(IntentBuilder('add')
@@ -70,28 +70,26 @@ class ListManager(MycroftSkill):
     def handle_add(self, message):
         """ Adds a new item to an existing list or creates a new one """
 
-        list_name = message.data.get('list_name')
-        item_name = message.data.get('item_name')
+        data = {'list_name': message.data.get('list_name'),
+                'item_name': message.data.get('item_name')}
 
-        if item_name:
-            # If the user specified an item_name, adds it to a list
-            if not self.db.list_exists(list_name):
-                data = {'list_name': list_name}
-                
+        if data['item_name']:
+            # If the user specified an item_name, add it to a list
+            if not self.db.list_exists(data['list_name']):
                 self.speak_dialog('list.not.found', data)
             else:
-                self.db.add_item(list_name, item_name)
-                data = {'item_name': item_name, 'list_name': list_name}
+                self.db.add_item(data['list_name'], data['item_name'])
 
                 self.speak_dialog('item.added', data)
-        else:
-            # Alternatively, simply creates a new list
-            if self.db.list_exists(list_name):
-                self.speak_dialog('list.found', {'list_name': list_name})
-            else:
-                self.db.add_list(list_name)
 
-                self.speak_dialog('list.added', {'list_name': list_name})
+        else:
+            # Alternatively, simply create a new list
+            if self.db.list_exists(data['list_name']):
+                self.speak_dialog('list.found', data)
+            else:
+                self.db.add_list(data['list_name'])
+
+                self.speak_dialog('list.added', data)
 
     @intent_handler(IntentBuilder('del')
         .require('del')
@@ -99,54 +97,49 @@ class ListManager(MycroftSkill):
         .require('list_name')
         .optionally('item_name'))
     def handle_del(self, message):
-        """ Removes a list or an item from an existing list """
+        """ Remove a list or an item from an existing list """
 
-        item_name = message.data.get('item_name')
-        list_name = message.data.get('list_name')
+        data = {'list_name': message.data.get('list_name'),
+                'item_name': message.data.get('item_name')}
 
-        if item_name:
-            # If the user specified an item_name, deletes it from the list
-            if not (self.db.list_exists(list_name) and \
-                    self.db.item_exists(list_name, item_name)):
-                data = {'item_name': item_name, 'list_name': list_name}
-                
+        if data['item_name']:
+            # If the user specified an item_name, delete it from the list
+            if not (self.db.list_exists(data['list_name']) and \
+                    self.db.item_exists(data['list_name'],
+                                        data['item_name'])):
                 self.speak_dialog('item.not.found', data)
-                        
             else:
-                if self.confirm_deletion(item_name):
-                    self.db.del_item(list_name, item_name)
-                    data = {'item_name': item_name, 'list_name': list_name}
-                    
+                if self.confirm_deletion(data['item_name']):
+                    self.db.del_item(data['list_name'],
+                                     data['item_name'])
+
                     self.speak_dialog('item.removed', data)
-                   
+
         else:
-            # Alternatively, simply creates a new list
-            if not self.db.list_exists(list_name):
-                data = {'item_name': item_name, 'list_name': list_name}
-                
+            # Alternatively, simply create a new list
+            if not self.db.list_exists(data['list_name']):
                 self.speak_dialog('list.not.found', data)
             else:
-                if self.confirm_deletion(list_name):
-                    self.db.del_list(list_name)
-                    data = {'item_name': item_name, 'list_name': list_name}
+                if self.confirm_deletion(data['list_name']):
+                    self.db.del_list(data['list_name'])
 
                     self.speak_dialog('list.removed', data)
 
     def string(self, lists):
-        """ Converts a python list into a string such as 'a, b and c' """
+        """ Convert a python list into a string such as 'a, b and c' """
 
         conj = self.translate_namedvalues('conj', delim=',')
         conj_spaced = ' {} '.format(conj.get('conj'))
         return ', '.join(lists[:-2] + [conj_spaced.join(lists[-2:])])
 
     def plural_singular_form(self, lists):
-        """ Returns singular or plural form as necessary """
+        """ Return singular or plural form as necessary """
 
         value = self.translate_namedvalues('list.or.lists', delim=',')
         return value.get('singular') if len(lists) == 1 else value.get('plural')
 
     def confirm_deletion(self, item):
-        """ Makes sure the user really wants to delete an item """
+        """ Make sure the user really wants to delete an item """
 
         resp = self.ask_yesno('confirm.deletion', data={'item': item})
         if resp == 'yes':
