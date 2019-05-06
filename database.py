f8b09e763237652b812a9168971fd25f863885f0
Luca Baffa <lb803@mailbox.org>
2019-05-06 19:14:58 +0100

    Added more methods
    Added methods to read, add and delete data to/from the json file.

--- database.py
+++ database.py
@@ -29,6 +29,7 @@ class Database:
             self.json_data = {}
             self.write_data()
 
+    ## FILE HANDLING
     def read_data(self):
         with open(self.JSON_PATH, 'r') as json_file:
             return json.load(json_file)
@@ -36,12 +37,39 @@ class Database:
     def write_data(self):
         with open(self.JSON_PATH, 'w') as json_file:
             json.dump(self.json_data, json_file)
-            
-    def list_empty(self, list):
-        return True if len(list) == 0 else False
-            
-    def list_exists(self, list):
-        return True if list in self.json_data.keys() else False
-        
+
+    ## CHECKS
     def item_exists(self, list, item):
         return True if item in self.json_data[list] else False
+
+    def list_empty(self, list):
+        return True if len(list) == 0 else False
+
+    def list_exists(self, list):
+        return True if list in self.json_data.keys() else False
+
+    ## READ
+    def read_items(self, list):
+        return self.json_data.get(list)
+        
+    def read_lists(self):
+        return list(self.json_data.keys())
+        
+    ## ADD
+    def add_item(self, list, item):
+        self.json_data[list].append(item)
+        self.write_data()
+    
+    def add_list(self, list):
+        self.json_data[list] = []
+        self.write_data()
+        
+    ## DEL
+    def del_item(self, list, item):
+        item_index = self.json_data[list].index(item)
+        del self.json_data[list][item_index]
+        self.write_data()
+        
+    def del_list(self, list):
+        del self.json_data[list]
+        self.write_data()
